8a76b9ed41b44dee1873100a6c4f1ca013d5711c
bkfwebdev <bryant.feld@gmail.com>
2017-03-28 11:41:37 -0400

    clean -w- comments

--- js/app.js
+++ js/app.js
@@ -1,14 +1,16 @@
-// get search string from user
-// encode search string for get request
-// issue get request with encoded string
-// log result
-// GET https://api.spotify.com/v1/search
+// Bryant Feld TechDegree Project 5
+//----------------------------------
+
+// Variables
 
 var resultsDisplayed = false;
 var noAlbumsFound = false;
 var blankSearch = $(".desc");
 
+// Functions
+
 function addListElement (albumTitle,albumArtist,albumArt){
+// adds albums to the page
 	 let newAlbum = document.createElement("li");
 	 let newDiv = document.createElement("div");
 	 let newImage = document.createElement("img");
@@ -30,84 +32,20 @@ function addListElement (albumTitle,albumArtist,albumArt){
 	albumList.appendChild(newAlbum);
 } 
 
-function processData(searchResponseObject){
-if (resultsDisplayed === true){clearLastSearch();}
-console.log(searchResponseObject);
-var loopEnd = searchResponseObject.albums.items.length;
-if (loopEnd === 0){
-	console.log("I got nothing for you son...");
-	iGotNothin($("#search").val());
-} else {
-for (index = 0; index < loopEnd; index ++){
-let currentAlbum = searchResponseObject.albums.items[index].name;
-let currentArtist = searchResponseObject.albums.items[index].artists[0].name;
-let currentImage = searchResponseObject.albums.items[index].images[0 ].url;
-addListElement(currentAlbum,currentArtist,currentImage);
+function iGotNothin(searchValue){
+// alerts the user than there were no search results
+	let noResults = document.createElement("li");
+	noResults.setAttribute("class","no-albums desc");
+	noResults.innerHTML = "<i class='material-icons icon-help'>help_outline</i>No albums found that match:"+searchValue+".</i>";
+	let myAnchor = document.getElementById("albums");
+	$(blankSearch).hide();
+	myAnchor.appendChild(noResults);
+	resultsDisplayed = true;
+	noAlbumsFound = true;
 }
-}
-resultsDisplayed = true;
-}
-
-function submitSearch (userInput){
-let queryString = "";
-let urlTarget = "https://api.spotify.com/v1/search"
-for (x=0; x< userInput.length; x++){
-	if (userInput.substr(x,1) !== " "){
-		queryString += userInput.substr(x,1);
-	} else {  
-		if (userInput.substr(x,1) == " "){
-			queryString += "%20";
-			}
-	}
-}
-queryString = "q=" + queryString + "&type=album";
-console.log(queryString);
-if (queryString !== ""){
-$.get(urlTarget,queryString,processData);
-}
-}
-
-$(".search-form").on("submit",function(event){
-	console.log("initiate function")
-	let testval = $("#search").val();
-	console.log(testval);
-	submitSearch(testval);
-	event.preventDefault();
-	})
-	
-getTracks();
-
-// track li loop
-// trackListData.items[0].name
-// trackListData.items[1].name etc
-
-
-// album-title path searchResponseObject.albums.items["0"].name
-// album-artist path searchResponseObject.albums.items["0"].artists["0"].name 
-// album-image path searchResponseObject.albums.items["0"].images["0"].url 
-
-
-function getTracks(){
-	//.albums.items[0].id
-	//.albums.items[1].id
-	// etc
-	// GET https://api.spotify.com/v1/albums/3tQd5mwBtVyxCoEo4htGAV/tracks
-	let tracksURL = "https://api.spotify.com"
-	let albumID = "3ckt1jRTh6Q08fUvEePI7B";
-	let targetURL = tracksURL + "/v1/albums/" + albumID + "/tracks?";
-	console.log(targetURL);
-	$.get(targetURL,albumID,logData);
-}
-	
-	function logData(data){
-		let trackListData = data;
-		console.log(trackListData);
-	}
-
-
-function clickAlbumEvent (){}
 
 function clearLastSearch (){
+// clears previous results so new results can be displayed
 	let currentSearch = $("li").not(".desc");
 	let maxIndex = currentSearch.length;
 	for (x = 0; x < maxIndex ; x++){
@@ -120,37 +58,47 @@ function clearLastSearch (){
 	resultsDisplayed = false;
 }
 
-function pageReset(){
-	clearLastSearch();
-		$(".desc").show();
-		}
-		
-function albumPage(){}
-
-function iGotNothin(searchValue){
-	let noResults = document.createElement("li");
-	noResults.setAttribute("class","no-albums desc");
-	noResults.innerHTML = "<i class='material-icons icon-help'>help_outline</i>No albums found that match:"+searchValue+".</i>";
-	let myAnchor = document.getElementById("albums");
-	$(blankSearch).hide();
-	myAnchor.appendChild(noResults);
-	resultsDisplayed = true;
-	noAlbumsFound = true;
+function processData(searchResponseObject){
+// display results of search
+if (resultsDisplayed === true){clearLastSearch();}
+console.log(searchResponseObject);
+var loopEnd = searchResponseObject.albums.items.length;
+if (loopEnd === 0){iGotNothin($("#search").val());
+} else {
+for (index = 0; index < loopEnd; index ++){
+let currentAlbum = searchResponseObject.albums.items[index].name;
+let currentArtist = searchResponseObject.albums.items[index].artists[0].name;
+let currentImage = searchResponseObject.albums.items[index].images[0 ].url;
+addListElement(currentAlbum,currentArtist,currentImage);
+}
+}
+resultsDisplayed = true;
 }
 
+function submitSearch (userInput){
+// submit search query to spotify api
+let queryString = "";
+let urlTarget = "https://api.spotify.com/v1/search"
+for (x=0; x< userInput.length; x++){
+	if (userInput.substr(x,1) !== " "){
+		queryString += userInput.substr(x,1);
+	} else {  
+		if (userInput.substr(x,1) == " "){
+			queryString += "%20";
+			}
+	}
+}
+queryString = "q=" + queryString + "&type=album";
+if (queryString !== ""){
+$.get(urlTarget,queryString,processData);
+}
+}
 
+$(".search-form").on("submit",function(event){
+	// get search target from user
+	let testval = $("#search").val();
+	submitSearch(testval);
+	event.preventDefault();
+	});
 
- /* reference
- <!--
-          <li>
-            <div class="album-wrap">
-              <img class="album-art" src="https://i.scdn.co/image/23837f31d4791981db85588e57a86cf2ce5b88e3">
-            </div>
-            <span class="album-title">Luck of the Draw</span>
-            <span class="album-artist">Bonnie Raitt</span>
-          </li>
-        -->
-		
-		.albums.items["0"].artists["0"].name
-		*/
 		
